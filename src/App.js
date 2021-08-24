7a71bf5bc27e69f10ef5c85ab9c99fb7717028fc
Lewis <hamiltonjlewis@gmail.com>
2021-08-23 20:33:47 -0700

    aligned to the center

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import Grid from "@material-ui/core/Grid";
 import { makeStyles } from "@material-ui/core/styles";
-import Container from "@material-ui/core/Container";
+import Box from "@material-ui/core/Box";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -18,25 +18,28 @@ export default function App() {
   const classes = useStyles();
 
   return (
-    <Container className={classes.root}>
-      <Grid justifyContent="Center">
-        <Typography variant="h1">{score}</Typography>
-
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={() => setScore(score + 1)}
-        >
-          +
-        </Button>
-        <Button
-          variant="contained"
-          color="secondary"
-          onClick={() => setScore(0)}
-        >
-          Reset
-        </Button>
+    <Box className={classes.root}>
+      <Grid container direction="column" alignItems="center">
+        <Grid item>
+          <Typography variant="h1">{score}</Typography>
+        </Grid>
+        <Grid item>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => setScore(score + 1)}
+          >
+            +
+          </Button>
+          <Button
+            variant="contained"
+            color="secondary"
+            onClick={() => setScore(0)}
+          >
+            Reset
+          </Button>
+        </Grid>
       </Grid>
-    </Container>
+    </Box>
   );
 }
