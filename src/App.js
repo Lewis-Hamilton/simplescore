aa15260d597134e83a511d343e3e4e55649a86e1
Lewis <hamiltonjlewis@gmail.com>
2021-08-23 19:01:25 -0700

    working score keeper

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React, { useState } from "react";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
 
 export default function App() {
-  return <p>HELLO</p>;
+  const [score, setScore] = useState(0);
+  return (
+    <div>
+      <Typography variant="h1">{score}</Typography>
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={() => setScore(score + 1)}
+      >
+        +
+      </Button>
+      <Button variant="contained" color="secondary" onClick={() => setScore(0)}>
+        Reset
+      </Button>
+    </div>
+  );
 }
