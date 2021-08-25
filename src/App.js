deb7a2c55968a1271357d34dd0259a5f21418454
Lewis <hamiltonjlewis@gmail.com>
2021-08-24 20:42:45 -0700

    centered everything

--- src/App.js
+++ src/App.js
@@ -4,12 +4,29 @@ import Typography from "@material-ui/core/Typography";
 import Grid from "@material-ui/core/Grid";
 import { makeStyles } from "@material-ui/core/styles";
 import Box from "@material-ui/core/Box";
+import Container from "@material-ui/core/Container";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
     background: "linear-gradient(45deg, #fe6b8b 30%, #ff8e53 90%)",
     height: "100vh",
+  },
+  text: {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+  },
+  grid: {
+    position: "absolute",
+    top: "90%",
+    left: "50%",
+    transform: "translatex(-50%)",
+  },
+  button: {
+    marginLeft: "10vw",
+    marginRight: "10vw",
   },
 }));
 
@@ -18,28 +35,32 @@ export default function App() {
   const classes = useStyles();
 
   return (
-    <Box className={classes.root}>
-      <Grid container direction="column" alignItems="center">
+    <Container className={classes.root}>
+      <Typography className={classes.text} variant="h1">
+        {score}
+      </Typography>
+      <Grid container direction="row" className={classes.grid}>
         <Grid item>
-          <Typography variant="h1">{score}</Typography>
+          <Button
+            variant="contained"
+            color="secondary"
+            onClick={() => setScore(0)}
+            className={classes.button}
+          >
+            Reset
+          </Button>
         </Grid>
         <Grid item>
           <Button
             variant="contained"
             color="primary"
             onClick={() => setScore(score + 1)}
+            className={classes.button}
           >
             +
           </Button>
-          <Button
-            variant="contained"
-            color="secondary"
-            onClick={() => setScore(0)}
-          >
-            Reset
-          </Button>
         </Grid>
       </Grid>
-    </Box>
+    </Container>
   );
 }
